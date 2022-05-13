74a74650e4a990512a9a02d1bf01fb4e3ff97f6a
Adris Jautakas <adrisj7@gmail.com>
2022-05-13 18:48:44 -0400

    Add user place in high/low score top label

--- UI/Highscore/HighScoreListUI.cs
+++ UI/Highscore/HighScoreListUI.cs
@@ -12,12 +12,32 @@ namespace CustomBeatmaps.UI.Highscore
             var highScores = CustomBeatmaps.ServerHighScoreManager.GetHighScores(beatmapKey);
             var lowScores = CustomBeatmaps.ServerHighScoreManager.GetLowScores(beatmapKey);
 
-            GUILayout.Label("<size=24>HIGH SCORES</size>");
+            int highScoreCount = highScores?.Count ?? 0;
+            int lowScoreCount = lowScores?.Count ?? 0;
+            int userHighPlace = -1,
+                userLowPlace = -1;
+
+            if (CustomBeatmaps.UserSession.LoggedIn)
+            {
+                var username = CustomBeatmaps.UserSession.Username;
+                if (highScores != null)
+                    userHighPlace = highScores.FindIndex(place => place.Key == username);
+                if (lowScores != null)
+                    userLowPlace = lowScores.FindIndex(place => place.Key == username);
+            }
+
+            // TODO (for fun) Color code 1st 2nd and 3rd place (gold, silver, bronze)
+            string highScoreRankLabel = userHighPlace != -1 ? $"Ranked {userHighPlace + 1} / {highScoreCount}" : $"{highScoreCount}";
+            string lowScoreRankLabel = userLowPlace != -1 ? $"Ranked {userLowPlace + 1} / {lowScoreCount}" : $"{lowScoreCount}";
+            string highScoreLabel = $"HIGH SCORES <size=16>({highScoreRankLabel})</size>";
+            string lowScoreLabel = $"LOW BALLERS <size=16>({lowScoreRankLabel})</size>";
+
+            GUILayout.Label($"<size=24>{highScoreLabel}</size>");
             if (!RenderScores(highScores, 0))
             {
                 GUILayout.Label("Can't find High Scores...");
             }
-            GUILayout.Label("<size=24>LOW BALLERS</size>");
+            GUILayout.Label($"<size=24>{lowScoreLabel}</size>");
             if (!RenderScores(lowScores, 1))
             {
                 GUILayout.Label("Can't find Low Scores...");
