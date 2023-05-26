3b602c4c846f0a8038e4ed54754f142d65144e53
Adris Jautakas <adrisj7@gmail.com>
2023-05-25 23:56:44 -0400

    Gamepad support for online playing

--- UI/PackageList/PackageListUI.cs
+++ UI/PackageList/PackageListUI.cs
@@ -2,40 +2,88 @@
 using System.Collections.Generic;
 using CustomBeatmaps.UISystem;
 using CustomBeatmaps.Util;
+using Rewired;
 using UnityEngine;
 
 namespace CustomBeatmaps.UI.PackageList
 {
     public static class PackageListUI
     {
+        private static Player _rewired;
+
         public static void Render(string header, List<PackageHeader> packageHeaders, int selectedHeaderIndex, Action<int> onPackageHeaderSelect)
         {
             var (scrollPos, setScrollPos) = Reacc.UseState(Vector2.zero);
+            var (prevSelectedHeaderIndex, setPrevSelectedHeaderIndex) = Reacc.UseState(-1);
+
+            Reacc.UseEffect(() =>
+            {
+                _rewired = ReInput.players.GetPlayer(0);
+                // The game does not use/set up these parameters so I set them up here
+                _rewired.controllers.maps.GetInputBehavior(0).buttonRepeatDelay = 0.5f;
+                _rewired.controllers.maps.GetInputBehavior(0).buttonRepeatRate = 8;
+            });
 
             GUILayout.Label(header);
             var curScrollPos = GUILayout.BeginScrollView(scrollPos);
+                bool anySelected = false;
+                Rect selectedRect = Rect.zero;
+                Rect firstRect = Rect.zero;
                 for (int i = 0; i < packageHeaders.Count; ++i)
                 {
                     var packageHeader = packageHeaders[i];
                     int indexToSelect = i;
-                    PackageEntryUI.Render(packageHeader, selectedHeaderIndex == i, () =>
+                    bool selected = selectedHeaderIndex == i;
+                    Rect r = PackageEntryUI.Render(packageHeader, selected, () =>
                     {
                         onPackageHeaderSelect?.Invoke(indexToSelect);
                     }, packageHeader.DownloadStatus);
+
+                    if (i == 0)
+                    {
+                        firstRect = r;
+                    }
+                    if (selected)
+                    {
+                        anySelected = true;
+                        selectedRect = r;
+                    }
                 }
             GUILayout.EndScrollView();
+            Rect scrollViewRect = GUILayoutUtility.GetLastRect();
+            if (Event.current.type == EventType.Repaint)
+            {
+                // Make sure our scroll pos encapsulates the rect
+                if (anySelected && prevSelectedHeaderIndex != selectedHeaderIndex)
+                {
+                    float viewMinY = firstRect.yMin + curScrollPos.y;
+                    float viewMaxY = firstRect.yMin + scrollViewRect.height + curScrollPos.y;
+                    float deltaY = 0;
+                    if (selectedRect.yMax < viewMinY)
+                    {
+                        deltaY = selectedRect.yMin - viewMinY;
+                    } else if (selectedRect.yMin > viewMaxY)
+                    {
+                        deltaY = selectedRect.yMax - viewMaxY;
+                    }
 
+                    curScrollPos.y += deltaY;
+                }
+
+                setPrevSelectedHeaderIndex(selectedHeaderIndex);   
+            }
             setScrollPos(curScrollPos);
+
 
             // Keyboard Shortcuts: Navigate packages with arrow keys
 
             if (GUIHelper.CanDoInput() && packageHeaders.Count > 1)
             {
-                if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+                if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) || _rewired.GetNegativeButtonRepeating("Vertical"))
                 {
                     onPackageHeaderSelect((selectedHeaderIndex + 1) % packageHeaders.Count);
                 }
-                else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+                else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || _rewired.GetButtonRepeating("Vertical"))
                 {
                     int ind = selectedHeaderIndex - 1;
                     if (ind < 0)
--- UI/PackageList/PlayButtonUI.cs
+++ UI/PackageList/PlayButtonUI.cs
@@ -1,14 +1,23 @@
-﻿using CustomBeatmaps.Util;
+﻿using CustomBeatmaps.UISystem;
+using CustomBeatmaps.Util;
+using Rewired;
 using UnityEngine;
 
 namespace CustomBeatmaps.UI.PackageList
 {
     public static class PlayButtonUI
     {
+        private static Player _rewired;
+
         public static bool Render(string labelBig, string labelSmall="")
         {
+            Reacc.UseEffect(() =>
+            {
+                _rewired = ReInput.players.GetPlayer(0);
+            });
+
             return GUILayout.Button($"<size=24>{labelBig}</size>\n{labelSmall}", GUILayout.Height(72), GUILayout.ExpandHeight(false)) 
-                   || (GUIHelper.CanDoInput() && (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.KeypadEnter)));
+                   || (GUIHelper.CanDoInput() && (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.KeypadEnter) || _rewired.GetButtonDown("Interact")));
         }
     }
 }
